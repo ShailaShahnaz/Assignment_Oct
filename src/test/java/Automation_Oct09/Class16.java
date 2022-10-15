abd475cba074640a0bf76e253ce071d22de6d6e4
ShailaShahnaz <shailashahnaz1@gmail.com>
2022-10-15 00:36:36 -0400

    My Frist Commit

---
+++ src/test/java/Automation_Oct09/Class16.java
@@ -0,0 +1,27 @@
+package Automation_Oct09;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Class16 {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver1 = new ChromeDriver();
+		driver1.get("https://www.wikipedia.org");
+		driver1.manage().window().maximize();
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver2 = new FirefoxDriver();
+		driver2.get("https://www.dictionary.com");
+		driver2.manage().window().maximize();
+		
+		driver1.quit();
+		driver2.quit();
+		
+	}
+
+}
---
+++ src/test/java/Automation_Oct09/Class7.java
@@ -0,0 +1,28 @@
+package Automation_Oct09;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Class7 {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://linkedin.com");
+		driver.manage().window().maximize();
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver1 = new FirefoxDriver();
+		driver1.manage().window().maximize();
+		driver1.get("https://info.com");
+				
+		driver.quit();
+		driver1.quit();
+
+	}
+
+}
